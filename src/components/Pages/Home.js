5d9e586de472b2bca19e282b1cba3bbf741e98b7
Emirkan Acar <nydaolian@gmail.com>
2020-04-04 20:19:02 +0300

    fix

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -33,7 +33,7 @@ class Home extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-md-10 mx-auto">
                                 <div className="site-heading">
-                                    <h1>Clean Blog</h1>
+                                    <h1>Blog</h1>
                                     <span className="subheading">A Blog Theme by Start Bootstrap</span>
                                 </div>
                             </div>
